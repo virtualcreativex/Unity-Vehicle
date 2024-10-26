dc88eaea88f791b9191a75f92e225cc9113f6215
Monica Yaneth Loeb Willes <manager@virtualcreativex.com>
2024-10-26 18:23:50 -0500

    Primer Ejercicio de Unity Pathways 02 - Unidad 01 - Vehicle. Terminado. - Primer Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Private variables
+    private float speed = 10.0f;
+
+    private float turnSpeed = 25.0f;
+
+    private float horizontalInput;
+
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+
+        //This is where we get player's input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // We'll move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // We turn the vehicle
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+    }
+}
